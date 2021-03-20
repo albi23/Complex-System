6f1886c457060d2f33d7796f60a88437995312ec
Albert Piekielny <albert.piekielny@blue-veery.com>
2021-03-21 00:51:21 +0100

    end

--- sheet1.py
+++ sheet1.py
@@ -8,8 +8,12 @@ import nltk
 from nltk.book import *
 from nltk.corpus import stopwords
 from nltk import ngrams
+import random
+import time
+import hashlib
+import itertools
 from functools import reduce
-from typing import List, T
+from typing import List, T, Dict
 
 import numpy as np
 import statistics as stc
@@ -78,7 +82,7 @@ def task5(list_x: List[T], list_y: List[T]):
     Write a program that for two lists x and y (possibly of different sizes) returns a list
     of elements that are common between the lists
     """
-    return list(set(list_x).intersection(list_y))
+    return list(set(list_x).intersection(set(list_y)))
 
 
 def task6(word: str):
@@ -123,7 +127,12 @@ def task9():
 
     for c in input_str:
         histogram[c] += 1
-    print({k: v for k, v in sorted(histogram.items(), key=lambda item: item[1])})
+    sorted_dict = {k: v for k, v in sorted(histogram.items(), key=lambda item: item[1], reverse=True)}
+    most_common = -1
+    for sign in sorted_dict.items():
+        if sign[1] >= most_common:
+            print(sign[0], end=',')
+            most_common = sign[1]
 
 
 def task10(to_convert: int):
@@ -223,7 +232,7 @@ def task16():
 #     Import all books from nktl.book. Construct a set of words that appear in in
 #     text6 (Monty Python) but do not appear in text7 (Wall Street Journal).
 #     """
-#     return set(text6.tokens).intersection(text7.tokens)
+#     return set(text6.tokens).intersection(set(text7.tokens))
 #
 #
 # def task19():
@@ -317,93 +326,183 @@ def task23(str_one: str, str_two: str) -> None:
         print("Jaccard similarity: ", (len(intersection) / float(len(union))))
 
 
-def task24() -> None:
-    """
-    (use NLTK) List all words in text1 with edit distance from the word dog smaller
-    than 4. Hint: you can safely reject all long words without computations (why?).
-    """
-    print(list(filter(lambda x: len(x) <= 3 or nltk.edit_distance(x, "dog") < 4, set(text1))))
+# def task24() -> None:
+#     """
+#     (use NLTK) List all words in text1 with edit distance from the word dog smaller
+#     than 4. Hint: you can safely reject all long words without computations (why?).
+#     """
+#     print(list(filter(lambda x: len(x) <= 3 or nltk.edit_distance(x, "dog") < 4, set(text1))))
 
 
-def task25() -> None:
-    """
-    (use NLTK) Let text1 - text9 be bags of words. Compute similarity between all
-    pairs of texts.
-    """
-    words_bags = list(map(lambda x: set(x), [text1, text2, text3, text4, text5, text6, text7, text8, text9]))
-    for a in words_bags:
-        for b in words_bags:
-            intersection = a.intersection(b)
-            union = a.union(b)
-            if len(union) == 0:
-                print("Can not calculate Jaccard similarity due tu |A u B| = 0")
-            else:
-                print("Jaccard similarity: ", (len(intersection) / float(len(union))))
+# def task25() -> None:
+#     """
+#     (use NLTK) Let text1 - text9 be bags of words. Compute similarity between all
+#     pairs of texts.
+#     """
+#     words_bags = list(map(lambda x: set(x), [text1, text2, text3, text4, text5, text6, text7, text8, text9]))
+#     for a in words_bags:
+#         for b in words_bags:
+#             intersection = a.intersection(b)
+#             union = a.union(b)
+#             if len(union) == 0:
+#                 print("Can not calculate Jaccard similarity due tu |A u B| = 0")
+#             else:
+#                 print("Jaccard similarity: ", (len(intersection) / float(len(union))))
+#
+#
+# def task26() -> None:
+#     """
+#     (use NLTK) Let us consider a metric space (S, d), where S is the set of words from
+#     text1 and d is the Hamming distance. Find diameter of (S, d).
+#     Notes: The diameter of a set in a metric space is the supremum of distances between its points
+#     """
+#     punctions = str.maketrans('', '', string.punctuation)
+#     stop_words = set(stopwords.words('english'))
+#     cleared_text = [word.translate(punctions) for word in text1 if word not in stop_words]
+#     none_empty_words = [x for x in set(cleared_text) if x != '']
+#     max_distance = 0
+#     hamming_distance = lambda x, y: sum(letter_left != letter_right for letter_left, letter_right in zip(x, y))
+#     for x_pair in none_empty_words:
+#         for y_par in none_empty_words:
+#             if x_pair != y_par:
+#                 distance = hamming_distance(x_pair, y_par) if len(x_pair) == len(y_par) else 0
+#                 if distance > max_distance:
+#                     max_distance = distance
+#
+#     print("Diameter for text1 is: " + str(max_distance))
 
 
-def task26() -> None:
-    """
-    (use NLTK) Let us consider a metric space (S, d), where S is the set of words from
-    text1 and d is the Hamming distance. Find diameter of (S, d).
-    Notes: The diameter of a set in a metric space is the supremum of distances between its points
-    """
-    punctions = str.maketrans('', '', string.punctuation)
-    stop_words = set(stopwords.words('english'))
-    cleared_text = [word.translate(punctions) for word in text1 if word not in stop_words]
-    none_empty_words = [x for x in set(cleared_text) if x != '']
-    max_distance = 0
-    hamming_distance = lambda x, y: sum(letter_left != letter_right for letter_left, letter_right in zip(x, y))
-    for x_pair in none_empty_words:
-        for y_par in none_empty_words:
-            if x_pair != y_par:
-                distance = hamming_distance(x_pair, y_par) if len(x_pair) == len(y_par) else 0
-                if distance > max_distance:
-                    max_distance = distance
-
-    print("Diameter for text1 is: " + str(max_distance))
-
-
-def task27() -> None:
-    """
-    (use NLTK) Construct a dictionary that assigns each pair of consecutive words
-    in text1 the Jaccard similarity between them.
-    """
-    punctions = str.maketrans('', '', string.punctuation)
-    stop_words = set(stopwords.words('english'))
-    cleared_text = [word.translate(punctions) for word in text1 if word not in stop_words]
-
-    def jaccard_similarity(x: set, y: set):
-        intersection = x.intersection(y)
-        union = x.union(y)
-        return 0 if len(union) == 0 else (len(intersection) / float(len(union)))
-
-    word_pair_on_jaccard_similarity = {}
-
-    for i in range(len(cleared_text) - 1):
-        key = cleared_text[i] + ':' + cleared_text[i + 1]
-        word_pair_on_jaccard_similarity[key] = jaccard_similarity(set(cleared_text[i]), set(cleared_text[i + 1]))
-    print(word_pair_on_jaccard_similarity)
+# def task27() -> None:
+#     """
+#     (use NLTK) Construct a dictionary that assigns each pair of consecutive words
+#     in text1 the Jaccard similarity between them.
+#     """
+#     punctions = str.maketrans('', '', string.punctuation)
+#     stop_words = set(stopwords.words('english'))
+#     cleared_text = [word.translate(punctions) for word in text1 if word not in stop_words]
+#
+#     def jaccard_similarity(x: set, y: set):
+#         intersection = x.intersection(y)
+#         union = x.union(y)
+#         return 0 if len(union) == 0 else (len(intersection) / float(len(union)))
+#
+#     word_pair_on_jaccard_similarity = {}
+#
+#     for i in range(len(cleared_text) - 1):
+#         key = cleared_text[i] + ':' + cleared_text[i + 1]
+#         word_pair_on_jaccard_similarity[key] = jaccard_similarity(set(cleared_text[i]), set(cleared_text[i + 1]))
+#     print(word_pair_on_jaccard_similarity)
 
 
-def task28() -> None:
-    """
-    (use NLTK). For two words v and w, let relative edit distance be the Levensthein
-    distance between v and w divided by the sum of lengths v and w. Find two different
-    words in text2 with minimal relative edit distance.
-    """
+# def task28() -> None:
+#     """
+#     (use NLTK). For two words v and w, let relative edit distance be the Levensthein
+#     distance between v and w divided by the sum of lengths v and w. Find two different
+#     words in text2 with minimal relative edit distance.
+#     Notes: Levenshtein distance between two words is the minimum number of single-character edits
+#     (insertions, deletions or substitutions) required to change one word into the other.
+#     """
+#     punctions = str.maketrans('', '', string.punctuation)
+#     stop_words = set(stopwords.words('english'))
+#     text_2_words = set(text2)
+#     cleared_text = [word.translate(punctions) for word in text_2_words if word not in stop_words]
+#     cleared_text.sort(key=len, reverse=True)
+#     cleared_text = cleared_text[:1000]
+#
+#     edit_distances = [(10_000, ""), (10_000, "")]
+#     for i in range(len(cleared_text)):
+#         for j in range(i + 1, len(cleared_text)):
+#             distance = nltk.edit_distance(cleared_text[i], cleared_text[j]) / float(
+#                 len(cleared_text[i]) + len(cleared_text[j]))
+#             if distance < edit_distances[0][0]:
+#                 edit_distances[0] = (distance, cleared_text[i] + ":" + cleared_text[j])
+#             elif distance < edit_distances[1][0]:
+#                 edit_distances[1] = (distance, cleared_text[i] + ":" + cleared_text[j])
+#     print(edit_distances)
 
 
-def task29() -> None:
+def task29(bit_string: str, r: int) -> None:
     """
     For a given bitstring b and a natural number r list all bitstrings b’, such that the
     Hamming distance between b and b’ is equal n.
+    Notes: Should be 'is equal r' instead 'n' ??? let's assume that 'n'
     """
+    result = []
+    cartesian_product = ["".join(map(str, x)) for x in itertools.product([0, 1], repeat=len(bit_string))]
+
+    for bit_str in cartesian_product:
+        distance = sum(w1 != w2 for w1, w2 in zip(bit_str, bit_string))
+        if distance == r:
+            result.append(bit_str)
+
+    print(result)
 
 
-def task30() -> None:
+def task30(some_string: str, k: int) -> None:
     """
     Construct a function that for a given string and a natural number k returns a set
     of all its k-shingles.
+    """
+    assert k > 0
+    tokens = some_string.split()
+    print([tokens[i:i + k] for i in range(len(tokens) - k + 1)])
+
+
+def task31() -> None:
+    """
+    Generate a set S of n random bitstrings of length 100. Find min x,y∈S sha-1(x||y), where
+    x||y denotes concatenation of bitstrings x and y. Estimate, what is the maximal n for
+    this task that can be handled by your computer?
+    Notes : x||y and also  y||x ??, b) I can leave my computer for 100 years to keep it count... ?
+    """
+
+    for test in range(1, 10):
+        start_time = time.time()
+        bit_strings = ["{0:b}".format(random.getrandbits(100)).zfill(100) for _ in range(test * 1000)]
+        y = hashlib.sha1(bytearray("".join(["1" for _ in range(200)]), "utf8")).digest()
+        for i in range(len(bit_strings)):
+            for j in range(i + 1, len(bit_strings)):
+                x = hashlib.sha1(bytearray(bit_strings[i] + bit_strings[j], "utf8")).digest()
+                y = min(x, y)
+        print("--- %s seconds --- %s" % (time.time() - start_time, test))
+        # print(f'Min: {y}')
+
+
+def task32() -> Dict:
+    """
+    (use NLTK). Let S1 , S2 , S3 be the texts of all words shorter than 8 letters from text1,
+    text2, text3, respectively. Compute signatures for S1 , S2 , S3 represented by 100
+    minhashes and then estimate Jaccard similarity between each pair of S1 , S2 , S3 .
+    """
+    texts = ['text1', 'text2', 'text3']
+    txt_1 = [t for t in text1 if len(t) > 8]
+    txt_2 = [t for t in text2 if len(t) > 8]
+    txt_3 = [t for t in text3 if len(t) > 8]
+    texts_set = [set(txt_1), set(txt_2), set(txt_3)]
+
+    def min_hash_signature(text_set: set) -> List[float]:
+        return sorted(hash(word) for word in text_set)[:100]
+
+    signatures = [min_hash_signature(s) for s in texts_set]
+    jaccards = []
+    for i in range(len(texts_set)):
+        for j in range(i + 1, len(texts_set)):
+            set_sign1 = set(signatures[i])
+            set_sign2 = set(signatures[j])
+            intersection = set_sign1.intersection(set_sign2)
+            union = set_sign1.union(set_sign2)
+            jaccards.append(len(intersection) / len(union))
+    return {
+        txt1 + ':' + txt2: jaccard
+        for ((txt1, txt2), jaccard) in zip(itertools.combinations(texts, 2), jaccards)
+    }
+
+
+def task33() -> None:
+    """
+    Compare the results from the previous exercise with the exact Jaccard similarity of
+    sets S 1 , S 2 , S 3 . What if random permutation of the characteristic matrix rows were
+    replaced with a random mapping?
     """
 
 
@@ -440,4 +539,10 @@ if __name__ == '__main__':
     # task24()
     # task25()
     # task26()
-    task27()
+    # task27()
+    # task28()
+    # task29("110", 2)
+    # task30("Construct a function that for a given string and a natural number k", 3)
+    # task31()
+    print(task32())
+    print()
