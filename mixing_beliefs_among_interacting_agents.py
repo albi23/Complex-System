962b6d781dec50ccbbfbebd21b5698158cd22ab2
Albert Piekielny <albert.piekielny@blue-veery.com>
2021-06-08 14:46:59 +0200

    1. figure 1

--- mixing_beliefs_among_interacting_agents.py
+++ mixing_beliefs_among_interacting_agents.py
@@ -15,9 +15,9 @@ import plotly.graph_objects as go
 from plotly.io import write_image
 
 
-def opinion_simulation_1(d: float = 0.5, N: int = 2000) -> None:
-    u: float = 0.26
-    time_unit = 2
+def opinion_simulation_1(d: float = 0.1, N: int = 1_000) -> None:
+    u: float = 0.5
+    time_unit = 1
     MCS = 50_000
 
     opinions: List[float] = [rand.random() for _ in range(N)]
@@ -44,6 +44,30 @@ def opinion_simulation_1(d: float = 0.5, N: int = 2000) -> None:
     create_charts(x, y)
 
 
+def opinion_simulation_2(d: float = 0.05, N: int = 1_000) -> None:
+    u: float = 0.5
+    time_unit = 1
+    MCS = 250
+
+    initial_opinions: List[float] = [rand.random() for _ in range(N)]
+    opinions_copy: List[float] = [op for op in initial_opinions]
+    x: List[int] = []
+    y: List[float] = []
+
+    for i in range(MCS):
+        # print("\r" + str((float(i + 1) / MCS) * 100), end="%")
+        for j in range(time_unit):
+            agent_1: int = rand.randint(0, N - 1)
+            agent_2: int = rand.randint(0, N - 1)
+            while agent_1 == agent_2:
+                agent_2 = rand.randint(0, N - 1)
+            if abs(opinions_copy[agent_1] - opinions_copy[agent_2]) < d:
+                opinions_copy[agent_1] = opinions_copy[agent_1] + u * (opinions_copy[agent_2] - opinions_copy[agent_1])
+                opinions_copy[agent_2] = opinions_copy[agent_2] + u * (opinions_copy[agent_1] - opinions_copy[agent_2])
+
+    create_charts2(initial_opinions, opinions_copy)
+
+
 def create_charts(x, y) -> None:
     # fig = go.Figure()
     df2 = dict({
@@ -65,9 +89,9 @@ def create_charts(x, y) -> None:
                           x=0.92,
                           y=1,
                           traceorder='normal',
-                          font=dict(size=13,color='black'),
+                          font=dict(size=13, color='black'),
                       )
-                )
+                      )
     fig['data'][0]['showlegend'] = True
 
     fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True, ticks='outside', tickwidth=2,
@@ -81,8 +105,48 @@ def create_charts(x, y) -> None:
     fig.show()
 
 
+def create_charts2(x, y) -> None:
+    import plotly.express as px
+    df2 = dict({'x': x, 'y': y, })
+
+    fig = go.Figure()
+    sc1 = go.Scatter(df2, x=x, y=y, mode='markers', marker=dict(size=8, color='black', symbol='cross-open'))
+    fig.add_trace(sc1)
+
+    help_fig = px.scatter(df2, x="x", y="y", trendline="ols")
+    x_trend = help_fig["data"][1]['x']
+    y_trend = help_fig["data"][1]['y']
+    fig.add_trace(go.Line(x=[0,1], y=[0, 1], line={'dash': 'dash','color': 'grey'}))
+    fig.add_trace(go.Line(x=[0.1,1], y=[0,0.9], line={'dash': 'dash','color': 'grey'}))
+    fig.add_trace(go.Line(x=[0,0.9], y=[0.1,1], line={'dash': 'dash','color': 'grey'}))
+    # fig.add_trace(go.Line(x=x_trend, y=y_trend, line={'dash': 'dash','color': 'grey'}))
+
+    fig.update_layout(
+        {'plot_bgcolor': 'rgb(255, 255, 255)', 'paper_bgcolor': 'rgb(255, 255, 255)'},
+        xaxis=dict(tick0=0, dtick=0.2),
+        yaxis=dict(tick0=0, dtick=0.2),
+    )
+#            ['solid', 'dot', 'dash', 'longdash', 'dashdot', 'longdashdot']
+
+    fig.update_xaxes(showgrid=True, showline=True, linewidth=1, linecolor='black', mirror=True,
+                     ticks='outside', tickwidth=2,
+                     gridcolor='black',
+                     ticklen=8, title='', title_font_size=20, title_font_color='black', color='black',
+                     tickfont=dict({'size': 13}))
+    fig.update_yaxes(showgrid=True, showline=True, gridwidth=0.5, linewidth=1, linecolor='black', mirror=True,
+                     ticks='outside', tickwidth=2,
+                     ticklen=8, title='', title_font_size=20, title_font_color='black', color='black',
+                     gridcolor='black',
+                     tickfont=dict({'size': 13}))
+    # fig.update_traces(marker=dict(size=8, color='black', symbol='cross-open'), mode='markers')
+
+    # write_image(fig, f"opinions.png")
+    fig.show()
+
+
 if __name__ == '__main__':
-    opinion_simulation_1()
+    # opinion_simulation_1()
+    opinion_simulation_2()
 
     #     import plotly.express as px
     #
